89294ff00c0846b0ced997e06dae2af42a2b1dd7
Lívio Lopes <antonio.lvio760@gmail.com>
2023-06-23 20:44:10 -0300

    feat: create middleware for validation of token

---
+++ src/middlewares/tokenValidator.js
@@ -0,0 +1,16 @@
+const { statusCode, statusMessage } = require('../utils/status.utils');
+
+const tokenValidator = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(statusCode.UNAUTHORIZED).json(statusMessage.UNAUTHORIZED_TOKEN_NOT_FOUND);
+  } 
+  const typeAuthorization = typeof authorization !== 'string';
+  const lengthAuthorization = authorization.length < 16;
+  if (lengthAuthorization || typeAuthorization) {
+    return res.status(statusCode.UNAUTHORIZED).json(statusMessage.UNAUTHORIZED_TOKEN_INVALID);
+  }
+  return next();
+};
+
+module.exports = tokenValidator;
