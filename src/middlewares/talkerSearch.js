bc9e0cf4ccd9f4824b31a7084ab5e33226c31bec
Lívio Lopes <antonio.lvio760@gmail.com>
2023-06-28 15:02:41 -0300

    feat: create middleware for validation on endpoint GET /talker/search

---
+++ src/middlewares/talkerSearch.js
@@ -0,0 +1,21 @@
+const { statusCode, statusMessage } = require('../utils/status.utils');
+const { readTalkers, 
+   talkerByRate, hasOnlyRate, hasRateValid } = require('../utils/talkers.utils');
+
+const talkerSearchValidator = async (req, res, next) => {
+  const { q, rate } = req.query;
+  if (hasRateValid(rate)) {
+    return res.status(statusCode.BAD_REQUEST).json(statusMessage.BAD_REQUEST_TALK_RATE_INVALID);
+  }
+  if (hasOnlyRate(q, rate)) {
+    const listTalkers = await talkerByRate(rate);
+    return res.status(statusCode.OK).json(listTalkers);
+  }
+  if (!q) {
+    const listTalkers = await readTalkers();
+    return res.status(statusCode.OK).json(listTalkers);
+  }
+  return next();
+};
+
+module.exports = talkerSearchValidator;
